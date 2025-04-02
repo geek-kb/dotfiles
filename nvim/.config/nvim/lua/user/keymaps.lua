e8aa4528cd851b45faacb03da223b43a1c0a9342
Itai Ganot <lel@lel.bz>
2025-04-02 16:27:51 +0300

    Update

--- nvim/.config/nvim/lua/user/keymaps.lua
+++ nvim/.config/nvim/lua/user/keymaps.lua
@@ -504,14 +504,15 @@ vim.cmd('source ' .. vim.fn.stdpath 'config' .. '/lua/user/search-replace.vim')
 -----------------------------
 --- Resize tree separator ---
 -----------------------------
-vim.keymap.set('n', '<leader>-', function()
+
+vim.keymap.set('n', '<leader>w-', function()
   local width = vim.g.nvim_tree_width or 30
   vim.g.nvim_tree_width = math.max(width - 5, 20)
-  require('nvim-tree').resize(vim.g.nvim_tree_width)
+  require('nvim-tree.view').resize(vim.g.nvim_tree_width)
 end, { noremap = true, silent = true, desc = 'Reduce tree size' })
 
-vim.keymap.set('n', '<leader>+', function()
+vim.keymap.set('n', '<leader>w=', function()
   local width = vim.g.nvim_tree_width or 30
   vim.g.nvim_tree_width = math.min(width + 5, 50)
-  require('nvim-tree').resize(vim.g.nvim_tree_width)
+  require('nvim-tree.view').resize(vim.g.nvim_tree_width)
 end, { noremap = true, silent = true, desc = 'Increase tree size' })
